a3b599361b03cb5de84b1d350f60ea23334feac0
OliverS87 <OliverSchliebs@Gmx.de>
2017-12-09 01:10:23 -0500

    Fixed module path

--- PanSNP.py
+++ PanSNP.py
@@ -1,7 +1,8 @@
 # Wrapper function to run pansnp
 from Pansnp import Pansnp_main as pm
 import sys
-
+import os
 if __name__ == '__main__':
     print(sys.path)
+    sys.path.append(os.path.join("Pansnp"))
     pm.pansnp_main(sys.argv)
